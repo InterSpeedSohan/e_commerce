8a83208f27cce0e1f8a5ed3224aa4c2a81f749ae
Sohan <interspeedsohan@gmail.com>
2022-05-16 00:56:14 +0600

    Initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const PrimaryColor = Color(0xFF0905A3);
+const PrimaryColorLight = Color(0xFFFFECDF);
+const PrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFFFA63E), Color(0xFF05167F)]
+);
+const SecondaryColor = Color(0xFF979797);
+const TextColor = Color(0xFF757575);
+
+const AnimationDuration = Duration(microseconds: 200);
